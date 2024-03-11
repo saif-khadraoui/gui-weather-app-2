a33304342d3695d58dea76cc71b83039c1cef1d9
Asad <asadkhan91003@gmail.com>
2024-03-11 16:27:07 +0000

    added login form basic structure

---
+++ client/src/components/loginForm/loginForm.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import styles from "./loginForm.module.css"
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+    // ##TODO login logic: such as validating the credentials
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
